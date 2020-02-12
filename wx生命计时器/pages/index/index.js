76ec30b4be23f94391cc07b3b629217e8928780c
your name <you email>
2020-02-12 12:34:35 +0800

    miniprogram

--- wx生命计时器/pages/index/index.js
+++ wx生命计时器/pages/index/index.js
@@ -1,60 +1,41 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+"use strict";
 Page({
-  // data: {
-  //   motto: 'Hello World',
-  //   userInfo: {},
-  //   hasUserInfo: false,
-  //   canIUse: wx.canIUse('button.open-type.getUserInfo')
-  // },
-  //事件处理函数
-  bindViewTap: function() {
-    // wx.navigateTo({
-    //   url: '../logs/logs'
-    // })
+  data: {
+    year: "2000",
+    mouth: "00",
+    arr: [],
+    mytimes: 0
   },
-  onLoad: function () {
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse){
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
-  },
- // getUserInfo: function(e) {
-    // console.log(e)
-    // app.globalData.userInfo = e.detail.userInfo
-    // this.setData({
-    //   userInfo: e.detail.userInfo,
-    //   hasUserInfo: true
-    // })
-  //}
-  bindDateChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+  chagtime: function (event) {
+    var value = event.detail.value;
     this.setData({
-      date: e.detail.value
-    })
+      year: value.split("-")[0],
+      mouth: value.split("-")[1]
+    });
+    this.settime();
   },
-})
+  clear: function () {
+    this.setData({
+      year: "",
+      mouth: "",
+      arr: [],
+      mytimes: 0
+    });
+  },
+  onShow: function () {
+    this.settime();
+  },
+  settime: function () {
+    console.log;
+    var _a = this.data, year = _a.year, mouth = _a.mouth;
+    var oldtime = mouth === "00" ? new Date(year).getTime() : new Date(year + "-" + mouth).getTime();
+    var newtime = new Date(new Date().getFullYear() + "-" + (new Date().getMonth() + 1)).getTime();
+    if (newtime > oldtime) {
+      var arr = newtime - oldtime;
+      var sums = Math.ceil(arr / (1000 * 60 * 60 * 24 * 31));
+      this.setData({
+        mytimes: sums
+      });
+    }
+  }
+});
